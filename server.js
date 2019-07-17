0a4453b5150ff176be3bc62ce5fc82ef29b1cf80
Charles Muzhanye <charles@Charless-iMac.local>
2019-07-17 18:13:48 +0200

    added functionality for getting data from multiple pages

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 const Nightmare = require('nightmare')
 const cheerio = require('cheerio')
-
+const vo = require('vo')
 const url = 'https://roi.aib.gov.uk/roi/PublicSearches/PublicSearch/'
 const nightmare = Nightmare({show: true})
 
-//Initial query
-nightmare
-.goto(url)
-.wait('body')
-//.click('button[name="SBForSearch"]
-.click('[name="SBForSearch"]')
-.wait('[name="PageSize"]')
-.evaluate(()=>document.getElementsByName('PageSize')[0].value = "Page250") //Set each page to be able to display 250 results
-.evaluate(()=> document.getElementsByName('Update')[0].click()) //update the amount of results returned
-.wait("table tr:nth-child(250)")//wait for the page to load 250 rows
-.click("[name='PageNextAction']")//go to next page
-.wait('input[name="PageNumber"][value="1"]')//wait for the pagenumber to change
-.evaluate(()=>document.querySelector('body'))
-.wait('table')
-.evaluate(()=>document.querySelector('body').innerHTML)
-.end()
-.then(response=> getData(response))
+vo(scrapePage)(
+    (err, results)=>{
+        return null
+    }
+)
+// nightmare
+// .goto(url)
+// .wait('body')
+// .click('[name="SBForSearch"]')
+// .wait('[name="PageSize"]')
+// .evaluate(()=>document.getElementsByName('PageSize')[0].value = "Page250") //Set each page to be able to display 250 results
+// .evaluate(()=> document.getElementsByName('Update')[0].click()) //update the amount of results returned
+// .wait("table tr:nth-child(250)")//wait for the page to load 250 rows
+// .click("[name='PageNextAction']")//go to next page
+// .wait('input[name="PageNumber"][value="1"]')//wait for the pagenumber to change
+// .evaluate(()=>document.querySelector('body'))
+// .wait('table') //wait for next page to load
+// .evaluate(()=>document.querySelector('body').innerHTML)
+// .end()
+// .then(response=> getData(response))
 
 //subsequent query
 
@@ -31,7 +34,7 @@ const getHeaders = (html)=>{
         //console.log($(element).text())
         headers.push($(element).text())
     })
-    console.log(headers)
+
     return headers
 }
 
@@ -53,9 +56,33 @@ const getData = (html)=>{
         data.push(item)
     })
 
-    console.log(data[249])
+    console.log(data[0], data.length)
 }
 
 function *scrapePage(){
+    let pages = []
 
+    yield nightmare
+        .goto(url)
+        .wait('body')
+        .click('[name="SBForSearch"]')
+        .wait('[name="PageSize"]')
+        .evaluate(()=>document.getElementsByName('PageSize')[0].value = "Page250") //Set each page to be able to display 250 results
+        .evaluate(()=> document.getElementsByName('Update')[0].click()) //update the amount of results returned
+        .wait("table tr:nth-child(250)")//wait for the page to load 250 rows
+    
+    let n = 0    
+    while (n<10) {
+        const page = yield nightmare.evaluate(()=>document.querySelector('body').innerHTML)
+        pages.push(page)
+        getData(page)
+        //goto nextpage
+        yield nightmare.click("[name='PageNextAction']")
+            .wait(`input[name="PageNumber"][value="${n}"]`)
+            .wait("table tr:nth-child(250)") //condition has to be changed depending on whether or not we are on last page
+        
+        n++
+    }
+    console.log("Pages", pages.length)
+    yield nightmare.end()
 } 
