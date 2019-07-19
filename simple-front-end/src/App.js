ae074f9835b3e24f3d71accee63eba7b003d35cb
Charles Muzhanye <charles@Charless-iMac.local>
2019-07-19 10:10:03 +0200

    added a simple button and menu to the front end

--- simple-front-end/src/App.js
+++ simple-front-end/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Button, Toolbar, AppBar, Typography} from '@material-ui/core'
+
+const getInformation = ()=>{
+  console.log('I was pressed')
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar>
+        <Toolbar>
+            <Typography>
+                Page Scraper
+            </Typography>
+        </Toolbar>
+      </AppBar>
+      <Button
+        onClick={getInformation}
+        color='primary'
+        variant="contained"
+      >
+        Get Information
+      </Button>
     </div>
   );
 }
+
+
 
 export default App;
