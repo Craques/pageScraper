8675496243402bce8ee57ce37b7f115ac142ddbd
Charles Muzhanye <charles@Charless-iMac.local>
2019-07-19 14:02:41 +0200

    added build folder to repository

---
+++ simple-front-end/build/precache-manifest.a78fcca9962ef3179964c9e94ce75c5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2af7b306e6e4bdcc30d54c6dc029b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8fc2e9a927229d58926",
+    "url": "/static/css/main.e7660143.chunk.css"
+  },
+  {
+    "revision": "517c01562137a7121625",
+    "url": "/static/js/2.2179d5f2.chunk.js"
+  },
+  {
+    "revision": "d8fc2e9a927229d58926",
+    "url": "/static/js/main.edb8b3ea.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
